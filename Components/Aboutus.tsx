9bbb50e97f4754ef740118cec17e3084158e8226
nooraizafshan <afshannoreen010@gmail.com>
2025-08-01 15:41:55 +0500

    j

--- Components/Aboutus.tsx
+++ Components/Aboutus.tsx
@@ -101,7 +101,7 @@ const AboutUs = () => {
                   paragraph
                 >
 <Typography variant="h6">
-  At <span style={{ color: "#1976D2" }}></span>, We specialize in  installation, maintenance, repair, and Service, of gas appliances, and  Air conditioning heeting cooling systems, and plumbing solutions <br/>
+   <span style={{ color: "#1976D2" }}></span>, We specialize in  installation, maintenance, repair, and Service, of gas appliances, and  Air conditioning heeting cooling systems, and plumbing solutions <br/>
   <span style={{ color: "#1976D2" }}></span> We are committed to delivering safe, efficient, and fully compliant solutions with a focus on customer satisfaction and long-term reliability.
 
 Your comfort and safety is our priority — We  deliver results 
